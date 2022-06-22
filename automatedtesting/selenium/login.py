ff811a0d0d7064b707e4cb6b303c48ea9cb7d056
Ryan <1ryanhart@gmail.com>
2022-06-22 09:32:45 +0100

    removed print statements

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -12,29 +12,29 @@ logging.basicConfig(
 options = ChromeOptions()
 options.add_argument("--headless") 
 driver = webdriver.Chrome(options=options)
-print ('INFO: Browser started successfully. Navigating to the demo page to login.')
-logging.info('INFO: Browser started successfully. Navigating to the demo page to login.')
+# print ('INFO: Browser started successfully. Navigating to the demo page to login.')
+logging.info('Browser started successfully. Navigating to the demo page to login.')
 driver.get('https://www.saucedemo.com/')
 
 # Start the browser and login with standard_user
 def login (user, password):
-    print ('INFO: Starting the browser...')
-    logging.info('INFO: Starting the browser...')
+    # print ('INFO: Starting the browser...')
+    logging.info('Starting the browser...')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
     driver.find_element_by_css_selector("input[id='login-button']").click()
-    print('INFO: Successfully logged in as ' + user )
-    logging.info('INFO: Successfully logged in as ' + user)
+    # print('INFO: Successfully logged in as ' + user )
+    logging.info('Successfully logged in as ' + user)
 
 def add_to_cart():
-    print('INFO: Adding all 6 items to cart')
-    logging.info('INFO: Adding all 6 items to cart')
+    # print('INFO: Adding all 6 items to cart')
+    logging.info('Adding all 6 items to cart')
     items = driver.find_elements_by_css_selector("button.btn_primary.btn_inventory")
 
     for item in items:
         product = item.get_property("name")
-        print('INFO: ' + product + ' added to the cart')
-        logging.info('INFO: ' + product + ' added to the cart')
+        # print('INFO: ' + product + ' added to the cart')
+        logging.info(product + ' added to the cart')
         item.click()
     cart_label = driver.find_element_by_css_selector('.shopping_cart_badge').text
     assert cart_label == '6'
@@ -42,13 +42,14 @@ def add_to_cart():
 
 def remove_from_cart():
     driver.find_element_by_css_selector("a[class='shopping_cart_link']").click()
-    print('INFO: Removing all 6 items to cart')
+    # print('INFO: Removing all 6 items to cart')
+    logging.info('Removing all 6 items to cart')
     items = driver.find_elements_by_css_selector("button.cart_button")
 
     for item in items:
         product = item.get_property("name")
-        print('INFO: '+ product +' removed from the cart')
-        logging.info('INFO: '+ product +' removed from the cart')
+        # print('INFO: '+ product +' removed from the cart')
+        logging.info(product +' removed from the cart')
         item.click()
     
 
